092cdc39f8bffdc26c6294b5d304d19d057f2410
Soravath Mengseng <soravathmengseng@Soravaths-MacBook-Air.local>
2019-11-08 23:31:10 -0500

    create user register sign in and out

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+
+//Database
+mongoose.connect(process.env.MONGO_URI,
+  {  useNewUrlParser: true,
+     useUnifiedTopology: true
+   })
+.then(() => console.log("DB is connected"))
+
+mongoose.connection.on('error', error => {
+  console.log(`DB connection error: ${error.message}`);
+})
+
+const postRoutes = require('./routes/post')
+const authRoutes = require('./routes/auth')
+
+
+// middleware
+app.use(morgan("dev"))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+
+app.use('/', postRoutes )
+app.use('/', authRoutes )
+
+
+
+
+
+
+
+const port = process.env.PORT || 8080;
+app.listen( port, () => { console.log('Node API is running..!');})
